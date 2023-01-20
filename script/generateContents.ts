14a566cde0f89afbd9cf29e4ea73a698c4290fe6
karashou <948618251@qq.com>
2023-01-20 23:51:16 +0800

    feat: css

--- script/generateContents.ts
+++ script/generateContents.ts
@@ -16,8 +16,21 @@ const template = `
   <meta charset="UTF-8" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>Document</title>
+  <link rel="stylesheet" href="https://unpkg.com/browse/reset.css@2.0.2/reset.css">
+  <style>
+    a,
+    a:link,
+    a:visited,
+    a:hover,
+    a:active {
+      text-decoration: none;
+      color: inherit;
+    }
+
+  </style>
+  <title>how-browsers-work</title>
 </head>
+
 <body>
   $$$
 </body>
