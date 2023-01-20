60e0cdb93a4f58b6baa94a5ea81ce83b92db73f1
karashou <948618251@qq.com>
2023-01-21 00:25:57 +0800

    fix

--- script/generateContents.ts
+++ script/generateContents.ts
@@ -77,7 +77,7 @@ function writeContent(fileList: string[]) {
   let str = "";
 
   fileList.forEach((file) => {
-    const anchorStr = `<a href="./src/${file}" target="_blank" rel="noopener noreferrer">${file}</a>
+    const anchorStr = `<a href="./src/${file}" target="_blank" rel="noopener noreferrer">${file}</a><br/>
     `;
     str += anchorStr;
   })
